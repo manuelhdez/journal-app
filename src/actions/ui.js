82d9536f446f3f2925dbec4c03dd989f0e9d72a5
Manuel Hernández <manuelhernandez@outlook.com>
2020-08-19 21:08:06 -0600

    Reducer for UI, Form errors

---
+++ src/actions/ui.js
@@ -0,0 +1,14 @@
+import { types } from "types/types";
+
+export const uiSetError = (msgError) => {
+  return {
+    type: types.uiSetError,
+    payload: msgError,
+  };
+};
+
+export const uiRemoveError = () => {
+  return {
+    type: types.uiRemoveError,
+  };
+};
